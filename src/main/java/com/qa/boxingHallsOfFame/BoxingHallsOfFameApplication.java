53ace3f34beedb6e344dad70ba13eec1c723d877
DineshQA1 <dinesh.vijayan@qa.com>
2021-05-10 11:50:12 +0100

    javax.validation

---
+++ src/main/java/com/qa/boxingHallsOfFame/BoxingHallsOfFameApplication.java
@@ -0,0 +1,13 @@
+package com.qa.boxingHallsOfFame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxingHallsOfFameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxingHallsOfFameApplication.class, args);
+	}
+
+}
